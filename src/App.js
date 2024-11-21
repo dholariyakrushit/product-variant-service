e54720b3356c00cdbafd7aa262b27327478efd19
Krushit Dholariya <krushit@gmail.com>
2024-11-21 09:18:14 +0530

    Feat : setup react and add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import MonkLogo from './assets/images/monk.svg';
+import AddProduct from './components/add-product/AddProduct';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <img className='logo-header' src={MonkLogo} alt='monk logo' />
+       <p className='title-header'>Monk Upsell & Cross-sell</p>
       </header>
+      <hr/>
+      <AddProduct/>
     </div>
   );
 }
---
+++ src/components/add-product/AddProduct.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './AddProduct.css'
+
+function AddProduct() {
+    return (
+        <div className='add-product-container'>
+            <div>
+                <h2>
+                    Add Products
+                </h2>
+                <div className='selected-product-section'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddProduct
